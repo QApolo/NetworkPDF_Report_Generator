a9ffdfd0a554247f20a8f0c4ff7cf9b489ea1892
criscastro <djcriz5@gmail.com>
2019-05-07 01:07:42 -0500

    -minor changes

--- Services/Mailmonitor/MAIlmonitor_test.py
+++ Services/Mailmonitor/MAIlmonitor_test.py
@@ -3,5 +3,5 @@ this module tests over mailmonitor utils
 """
 from Services.Mailmonitor.SMTP import SMTPclient
 if __name__ == '__main__':
-    server = SMTPclient("198.168.0.15")
+    server = SMTPclient("localhost")
     server.sendmail("elcris@losanciosos.com", "elkai@losanciosos.com","correo de pruevba python")
