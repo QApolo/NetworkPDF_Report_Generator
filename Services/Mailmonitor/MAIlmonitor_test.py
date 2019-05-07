bcfda3b72cc41f242e001512b9a11aa7f7b20498
criscastro <djcriz5@gmail.com>
2019-05-07 13:11:38 -0500

    -send and receive tests implemented

--- Services/Mailmonitor/MAIlmonitor_test.py
+++ Services/Mailmonitor/MAIlmonitor_test.py
@@ -1,7 +1,48 @@
 """
 this module tests over mailmonitor utils
 """
+import sys, getopt
+
+from Services.Mailmonitor.IMAP import IMAPclient
 from Services.Mailmonitor.SMTP import SMTPclient
-if __name__ == '__main__':
+
+
+def test_send(sender, receiver, message):
     server = SMTPclient("localhost")
-    server.sendmail("elcris@losanciosos.com", "elkai@losanciosos.com","correo de pruevba python")
+    server.sendmail(sender, receiver, message)
+    server.close()
+
+def test_receive(user,passw):
+    server = IMAPclient("localhost")
+    server.fetch_mail(user, passw)
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hsrm:")
+        print(opts)
+        print(args)
+    except getopt.GetoptError:
+        print('usage MAILmonitor_test.py -option <args>*', "-s <")
+        sys.exit(2)
+    for opt, arg in opts:
+        print(f"opt {opt} arg {arg}")
+        if opt == '-h':
+            print('usage MAILmonitor_test.py -option <args>*')
+            sys.exit()
+        elif opt in ("-s", "--ifile"):
+            test_send(args[0], args[1], args[2])
+        elif opt in ("-r", "--ofile"):
+            test_receive(args[0], args[1])
+            pass
+        elif opt in ("-m", "--ofile"):
+            outputfile = arg
+    print('Input file is "', inputfile)
+    print('Output file is "', outputfile)
+
+
+if __name__ == '__main__':
+    # server = SMTPclient("localhost")
+    # server.sendmail("elcris@losanciosos.com", "elkai@losanciosos.com","correo de pruevba python")
+    main(sys.argv[1:])
